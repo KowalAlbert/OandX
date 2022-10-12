1d5d2431089a7b2f502bd41adba45e94b06c86db
Albert Kowal <kowal.albert.90@gmail.com>
2022-10-12 11:41:57 +0200

    Start

---
+++ src/test/java/com/oandx/OandXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oandx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OandXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
